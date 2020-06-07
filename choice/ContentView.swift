2f20f7fc64315a98ac5d174be54c6a01390a9492
ItsJasonPan <yingjiap@andrew.cmu.edu>
2020-06-07 01:35:12 -0400

    Initial Commit

---
+++ choice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  choice
+//
+//  Created by 潘英剑 on 6/7/20.
+//  Copyright © 2020 潘英剑. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
